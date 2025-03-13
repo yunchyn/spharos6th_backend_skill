e3425056c6b1c7d368ba2184f596530e910ecb72
unknown <doeh2580@gmail.com>
2025-03-13 09:52:02 +0900

    [init] spring boot initial

---
+++ src/main/java/com/yunchyn/spharos6th/Spharos6thApplication.java
@@ -0,0 +1,13 @@
+package com.yunchyn.spharos6th;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spharos6thApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spharos6thApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yunchyn/spharos6th/Spharos6thApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yunchyn.spharos6th;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spharos6thApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
